8c37a4ef56410ffeef0559a9b44bfe3ecfbe0178
Amirjon Ulmasov <amirjonu@nycstudents.net>
2022-09-29 19:06:54 -0400

    finished lab

--- src/RestaurantRunner.java
+++ src/RestaurantRunner.java
@@ -1,2 +1,6 @@
 public class RestaurantRunner {
+    public static void main(String[] args){
+        restaurant res1= new restaurant("PIZZARIA", 5, "Italian");
+        res1.diningPlace();
+    }
 }
--- src/restaurant.java
+++ src/restaurant.java
@@ -1,3 +1,18 @@
 public class restaurant {
+    private int rating;
+    private String name;
+    private String cuisine;
+
+    public restaurant(String resName, int resRate, String resCuisine){
+        rating= resRate;
+        name=resName;
+        cuisine= resCuisine;
+
+    }
+
+    public void diningPlace(){
+        System.out.println("Hello our restaurant is named "+name+" and our cuisine is "+cuisine+" and our rating is "+rating+"/5.");
+    }
+
 
 }
